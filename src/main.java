a900b3f986655e23003d32ab29b297e445e597ca
Kancharla Shivani <shivanikancharla1810@gmail.com>
2025-03-12 20:49:42 +0530

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,14 @@
+package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("2211CS050046");
+		SpringApplication.run(Main.class, args);
+	}
+
+}
